9ada8d0c7d50558d4d9e11a4fecfc537c343b321
loaiabdalslam <loaiabdalslam@gmail.com>
2019-06-26 23:06:25 +0200

    Added AltList Array

--- slide-show-module.js
+++ slide-show-module.js
@@ -1,40 +1,57 @@
-const slideMeBabe = (linksArray, startingIndex, endingIndex, className,imgAlt,width, height,  timer) => {
+
+
+module.exports = {
+ 
+
+ slideMeBabe: function(linksArray, startingIndex, endingIndex, className,imgAlt,width, height,  timer) {
   
-  const links = linksArray
-  let sliderStartingIndex = 0
+  const links = linksArray;
+  const AltArray = imgAlt;
+
+  let sliderStartingIndex = 0;
   
   if (startingIndex){
-    sliderStartingIndex = startingIndex
+    sliderStartingIndex = startingIndex;
   } 
   
+  // Let's Make it Cool 
   
   for (let index = 0; index < links.length; index++){
+
     let img = document.createElement("img")
     img.setAttribute("src", links[index])
     img.style.display = "none";
     img.setAttribute("width", width ||380);
     img.setAttribute("height", height || 380);
-    img.setAttribute("alt", imgAlt || "broken-img");
+    img.setAttribute("alt", AltArray[index] || "broken-img");
     document.getElementById(className || "images-container").appendChild(img)
-}
 
-function updateNodes() {
+ }
+
+
+  setInterval(module.exports.inter, timer || 5000) // i love number 5 and i'm sorry for that !
+
+},
+
+
+// This Function for Updating Node .
+updateNodes: function() {
     const children = document.getElementById(className).childNodes;
     children.forEach(element => element.style.display = "none")
     children[sliderStartingIndex].style.display = "block";
-}
+},
 
-function inter() {
+// This FUnction for Using For Sliding 
+inter: function() {
   if (sliderStartingIndex === endingIndex || linksArray-1){
-    updateNodes()
+    module.exports.updateNodes()
     sliderStartingIndex = 0
   }else{
-    updateNodes()
-    sliderStartingIndex++
+    module.exports.updateNodes()
+    sliderStartingIndex ++
   }
 }
-setInterval(inter, timer || 2000)
-  
+
+
+
 }
-
-
